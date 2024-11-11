23e0a44b8da5594f0ad92a74a963796223ab09cc
Bruno77749 <brunoinacioamorim@gmail.com>
2024-11-11 10:33:54 -0300

    Delete site tcc directory

--- site tcc/entra.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-
-    document.getElementById('login-form').style.display = 'none';
-    document.getElementById('welcome-message').style.display = 'block';
-});
